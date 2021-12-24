7b34a9af468a78a8aa12542da544f0bd3234593f
Bro Developer <out455@ya.ru>
2021-12-24 17:35:46 +0200

    Update windows_login.py
    #change email and password to your data
    #this script will generate a token for your account that will expire in 28 days

--- windows_login.py
+++ windows_login.py
@@ -1,8 +1,10 @@
 import pprint
 import requests
 
+#change email and password to yours data
 email = "valery3@gmail.com"
 password = "valerypasswd"
+
 pload = {"username":email,"password":password}
 response = requests.post('http://136.244.82.142:8000/login', data=pload)
 print ('Status:',response.status_code)

beadc3ad9a3f6d4fd711f3d1d1d071245ddc12e4
Bro Developer <out455@ya.ru>
2021-12-24 17:57:47 +0200

    Update windows_ip_update.py

--- windows_ip_update.py
+++ windows_ip_update.py
@@ -4,11 +4,11 @@ import requests
 #CHANGE TOKEN, EMAIL, APIKEY, DOMAIN,IPADDRESS TO YOUR DATA
 #MAKE THIS SCRIPT AS A CRON JOB
 
-token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo1LCJleHAiOjE2NDI3MzQ5Mjl9.SyiaAglQV-MkN6ktyi9AHctPY7LPQkL7FdBCPJtxjDo"
-email = "valery3@gmail.com"
-apikey = "s1UuaB2AFiz"
-domain = "volodimirthegreat.com"
-ipaddress = "1.2.3.55"
+token= "yourtokenhere"
+email = "name@gmail.com"
+apikey = "yourapikey"
+domain = "yourdomain.com"
+ipaddress = "your ip address here" #for example: "1.1.1.1"
 
 
 pload = {"email":email,"apikey":apikey,"domain":domain,"ipaddress":ipaddress}

454acf118941f29e21e0ca6f567cd840cb288aab
Bro Developer <out455@ya.ru>
2021-12-24 17:38:14 +0200

    add domain to the db
    #CHANGE TOKEN, EMAIL, APIKEY, DOMAIN,IPADDRESS TO YOUR DATA

--- windows_add.py
+++ windows_add.py
@@ -1,12 +1,14 @@
 import pprint
 import requests
 
-token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo1LCJleHAiOjE2NDI3MzQwMTV9.95qoHlW42DPoHXZnnvE2lkDjLUGFgC2_-wQZFfIULwM"
+#CHANGE TOKEN, EMAIL, APIKEY, DOMAIN,IPADDRESS TO YOUR DATA
 
+token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo1LCJleHAiOjE2NDI3MzQwMTV9.95qoHlW42DPoHXZnnvE2lkDjLUGFgC2_-wQZFfIULwM"
 email = "valery3@gmail.com"
 apikey = "s1UuaB2AFiz"
 ipaddress = "3.3.3.3"
 domain= "volodimirthegreat2.com"
+
 pload = {"email":email,"apikey":apikey,"ipaddress":ipaddress,"domain":domain}
 headers = {"Authorization": f"Bearer {token}"}
 response = requests.post('http://136.244.82.142:8000/add', json=pload,headers = headers)

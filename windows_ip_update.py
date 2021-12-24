f9143c02f2ae6312bca6ab884c503c71acc5edb0
Bro Developer <out455@ya.ru>
2021-12-24 17:37:20 +0200

    Update windows_ip_update.py
    #CHANGE TOKEN, EMAIL, APIKEY, DOMAIN,IPADDRESS TO YOUR DATA
    #MAKE THIS SCRIPT AS A CRON JOB

--- windows_ip_update.py
+++ windows_ip_update.py
@@ -1,11 +1,16 @@
 import pprint
 import requests
-token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo1LCJleHAiOjE2NDI3MzQ5Mjl9.SyiaAglQV-MkN6ktyi9AHctPY7LPQkL7FdBCPJtxjDo"
 
+#CHANGE TOKEN, EMAIL, APIKEY, DOMAIN,IPADDRESS TO YOUR DATA
+#MAKE THIS SCRIPT AS A CRON JOB
+
+token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo1LCJleHAiOjE2NDI3MzQ5Mjl9.SyiaAglQV-MkN6ktyi9AHctPY7LPQkL7FdBCPJtxjDo"
 email = "valery3@gmail.com"
 apikey = "s1UuaB2AFiz"
 domain = "volodimirthegreat.com"
 ipaddress = "1.2.3.55"
+
+
 pload = {"email":email,"apikey":apikey,"domain":domain,"ipaddress":ipaddress}
 headers = {"Authorization": f"Bearer {token}"}
 response = requests.post('http://136.244.82.142:8000/update', json=pload, headers=headers)

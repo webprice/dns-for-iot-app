5e961c2147431d43223deecaaae1fa12106318fa
Bro Developer <out455@ya.ru>
2021-12-24 17:38:58 +0200

    /check
    #CHANGE TOKEN, EMAIL, APIKEY TO YOUR DATA
    #check your account's data and domains

--- windows_check_account.py
+++ windows_check_account.py
@@ -1,7 +1,10 @@
 import pprint
 import requests
-token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo1LCJleHAiOjE2NDI3MzQwMTV9.95qoHlW42DPoHXZnnvE2lkDjLUGFgC2_-wQZFfIULwM"
 
+
+#CHANGE TOKEN, EMAIL, APIKEY TO YOUR DATA
+
+token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo1LCJleHAiOjE2NDI3MzQwMTV9.95qoHlW42DPoHXZnnvE2lkDjLUGFgC2_-wQZFfIULwM"
 email = "valery3@gmail.com"
 apikey = "s1UuaB2AFiz"
 

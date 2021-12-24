5a21d0f87040011759c2b8a671e8543e217ba08a
Bro Developer <out455@ya.ru>
2021-12-24 17:40:12 +0200

    /DELETE - FOR DOMAIN REMOVAL FROM DB AND ACCOUNT
    #CHANGE TOKEN, EMAIL, APIKEY,DOMAIN_REMOED TO YOUR DATA

--- windows_delete_domain.py
+++ windows_delete_domain.py
@@ -1,10 +1,14 @@
 import pprint
 import requests
-token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo1LCJleHAiOjE2NDI3MzQwMTV9.95qoHlW42DPoHXZnnvE2lkDjLUGFgC2_-wQZFfIULwM"
 
+#CHANGE TOKEN, EMAIL, APIKEY,DOMAIN_REMOED TO YOUR DATA
+token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo1LCJleHAiOjE2NDI3MzQwMTV9.95qoHlW42DPoHXZnnvE2lkDjLUGFgC2_-wQZFfIULwM"
 email = "valery3@gmail.com"
 apikey = "s1UuaB2AFiz"
+
+#DOMAIN YOU WANT TO REMOVE:
 domain_removed = "volodimirthegreat2.com"
+
 pload = {"email":email,"apikey":apikey,"domain_removed":domain_removed}
 headers = {"Authorization": f"Bearer {token}"}
 response = requests.delete('http://136.244.82.142:8000/delete/', json=pload,headers = headers)

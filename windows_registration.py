1a5ff91fed934447d05a68cc47aa4bfc6764b9f7
Bro Developer <out455@ya.ru>
2021-12-24 17:34:52 +0200

    Update windows_registration.py
    #change email and password to yours data

--- windows_registration.py
+++ windows_registration.py
@@ -1,17 +1,14 @@
 import pprint
 import requests
 
-#register
-#device_ip = get('http://fra.rastem.com.ua/ip.php').text.format()
-#print(device_ip)
-#device_ip = "153.45.23.53"
+#change email and password to yours data
 email = "valeryboss@gmail.com"
 password = "valerypasswd"
+
+
 dictToSend = {'email':email,'password': password}
 
 res = requests.post('http://136.244.82.142:8000/register', json=dictToSend)
 print ('response from server:',res.status_code,":", res.text)
 print("res,json",res.json())
 pprint.pprint(res.json())
-# print ('response from server:',res.text)
-# dictFromServer = res.json()

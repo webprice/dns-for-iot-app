ae88d5a40c1dcd9db4a51b65378b2a4e6a079224
Bro Developer <out455@ya.ru>
2021-12-24 17:56:46 +0200

    Update windows_login.py

--- windows_login.py
+++ windows_login.py
@@ -2,8 +2,8 @@ import pprint
 import requests
 
 #change email and password to yours data
-email = "valery3@gmail.com"
-password = "valerypasswd"
+email = "yourname@gmail.com"
+password = "yourpassword"
 
 pload = {"username":email,"password":password}
 response = requests.post('http://136.244.82.142:8000/login', data=pload)

c9da502024cd8077bb3d016c0ff6b18d6e367783
Bro Developer <out455@ya.ru>
2021-12-24 17:58:18 +0200

    Update windows_delete_domain.py

--- windows_delete_domain.py
+++ windows_delete_domain.py
@@ -2,12 +2,12 @@ import pprint
 import requests
 
 #CHANGE TOKEN, EMAIL, APIKEY,DOMAIN_REMOED TO YOUR DATA
-token= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo1LCJleHAiOjE2NDI3MzQwMTV9.95qoHlW42DPoHXZnnvE2lkDjLUGFgC2_-wQZFfIULwM"
-email = "valery3@gmail.com"
-apikey = "s1UuaB2AFiz"
+token= "your token here"
+email = "name@gmail.com"
+apikey = "your apikey here"
 
 #DOMAIN YOU WANT TO REMOVE:
-domain_removed = "volodimirthegreat2.com"
+domain_removed = "your domain here"
 
 pload = {"email":email,"apikey":apikey,"domain_removed":domain_removed}
 headers = {"Authorization": f"Bearer {token}"}
